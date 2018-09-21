d2acf54c8c8d60a6ab8d8916d14b9a100660e32a
Jan Blasko <honza@lucien144.net>
2018-09-22 09:21:26 +1000

    XO fixes
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- sync.js
+++ sync.js
@@ -8,7 +8,7 @@ const diskdb = require('diskdb');
 
 const db = diskdb.connect('./db', ['videos']);
 
-(async () => {
+const syncData = async () => {
 	let results = false;
 	let publishedBefore = null;
 	let options = {};
@@ -19,25 +19,26 @@ const db = diskdb.connect('./db', ['videos']);
 				order: 'date',
 				publishedBefore
 			};
+			/* eslint-disable no-await-in-loop */
 			results = await API.search(null, 100, options);
 		} catch (error) {
 			console.error(error);
 			return;
 		}
 		results.forEach(video => {
-			if (video.title !== undefined) {
-				if (/^Joe Rogan Experience #(\d*)(\s?[-]{0,}\s?)(.*)$/i.test(video.title)) {
-					db.videos.save(video);
-					console.log(video.title);
-					console.log(video.publishedAt);
-					console.log('---');
-					publishedBefore = video.publishedAt;
-					publishedBefore.setSeconds(publishedBefore.getSeconds() - 1);
-					publishedBefore = publishedBefore.toISOString();
-				}
-			} else {
+			if (video.title === undefined) {
 				results = false;
+			} else if (/^Joe Rogan Experience #(\d*)(\s?[-]{0,}\s?)(.*)$/i.test(video.title)) {
+				db.videos.save(video);
+				console.log(video.title);
+				console.log(video.publishedAt);
+				console.log('---');
+				publishedBefore = video.publishedAt;
+				publishedBefore.setSeconds(publishedBefore.getSeconds() - 1);
+				publishedBefore = publishedBefore.toISOString();
 			}
 		});
 	} while (results);
-})();
+};
+
+syncData();
