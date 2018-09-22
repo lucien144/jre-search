79367321c0bfe71cdef8dd33aab7aee630348ad8
Jan Blasko <honza@lucien144.net>
2018-09-23 00:14:12 +1000

    Pos replaced with Compendium
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- parse.js
+++ parse.js
@@ -2,73 +2,95 @@ const l = console.log;
 
 const S = require('string');
 const diskdb = require('diskdb');
-const pos = require('pos');
+const compendium = require('compendium-js');
 
 const db = diskdb.connect('./db', ['videos']);
 const videos = db.videos.find();
 
 const keywords = {};
+const hosts = {};
+const tags = {};
+const titles = [];
+
+const getId = title => {
+	const id = S(title).trim().s.toLowerCase();
+	return id.replace(/[^\w]/gi, '');
+};
+
+const saveKeyword = (keyword, pool) => {
+	const id = getId(keyword);
+	if (pool[id] === undefined) {
+		pool[id] = {
+			original: keyword,
+			count: 1
+		};
+	} else {
+		pool[id].count++;
+	}
+};
+
+const parseEntities = description => {
+	const anal = compendium.analyse(description);
+	try {
+		anal[0].entities.forEach(entity => {
+			saveKeyword(entity.raw, keywords);
+			description = description.replace(entity.raw, '');
+		});
+	} catch (error) {
+		// Console.warn(error);
+	}
+	return description;
+};
+
+const parseQuotes = description => {
+	const regex = new RegExp('("(?<title>.*?)")|(\'[^s](?<title2>.*?)\')', 'gms');
+	let match;
+	while ((match = regex.exec(description)) !== null) {
+		const title = match.groups.title || match.groups.title2;
+		saveKeyword(title, keywords);
+		description = description.replace(match[0], '');
+	}
+	return description;
+};
+
+const parseTitle = title => {
+	const matches = title.match(/^Joe Rogan Experience #(?<episode>\d*)(\s?[-]{0,}\s?)(?<hosts>.*?)(?<part>\s?\(part\s[0-9]{1,}\))?$/i);
+	if (matches) {
+		return {
+			episode: Number(matches.groups.episode),
+			hosts: matches.groups.hosts.split(/[,&]+/).map(el => S(el).trim().s)
+		};
+	}
+	return false;
+};
 
 videos.forEach(video => {
-	const words = new pos.Lexer().lex(video.description);
-	const tagger = new pos.Tagger();
-	const taggedWords = tagger.tag(words);
+	let {title, description} = video;
+	title = parseTitle(title);
+	if (title && titles.filter(val => val.episode === title.episode).length === 0) {
+		titles.push(title);
+		title.hosts.forEach(host => {
+			saveKeyword(host, hosts);
+			description = description.replace(host, '');
+		});
 
-	let name = '';
-	let tags = [];
-	for (const i in taggedWords) {
-		if ({}.hasOwnProperty.call(taggedWords, i)) {
-			const taggedWord = taggedWords[i];
-			const word = taggedWord[0];
-			const tag = taggedWord[1];
-			let found = false;
+		description = parseQuotes(description);
+		description = parseEntities(description);
 
-			// Foreign Word
-			if (tag === 'FW') {
-				name = `${name} ${word}`;
-				tags.push(tag);
-				found = true;
-			}
-
-			// Adjective (big, ...)
-			if (tag === 'JJ') {
-				name = `${name} ${word}`;
-				tags.push(tag);
-				found = true;
-			}
-
-			if (tag === 'NN' || tag === 'NNP' || tag === 'NNPS' || tag === 'NNS') {
-				name = `${name} ${word}`;
-				tags.push(tag);
-				found = true;
-			}
-			if (!found && name !== '') {
-				let id = S(name).trim().s.toLowerCase();
-				id = id.replace(/[^\w]/gi, '');
-				let val = S(name).trim().s;
-				val = val.replace(/[^\w\s]/gi, '');
-				if (keywords[id] === undefined) {
-					keywords[id] = {
-						original: name,
-						name: val,
-						tags: tags,
-						count: 1
-					};
-				} else {
-					keywords[id].count++;
+		let noun = '';
+		compendium.analyse(description).forEach(anal => {
+			anal.tokens.forEach(token => {
+				let found = false;
+				if (['NN', 'NNS', 'JJ'].indexOf(token.pos) > -1) {
+					noun += ' ' + token.raw;
+					found = true;
 				}
-				name = '';
-				tags = [];
-			}
-		}
+				if (noun !== '' && !found) {
+					saveKeyword(noun, keywords);
+					saveKeyword(noun, tags);
+					noun = '';
+				}
+			});
+		});
 	}
 });
-
-for (const key in keywords) {
-	if (keywords[key].count > 10) {
-		l(keywords[key]);
-	}
-}
-
-l(Object.keys(keywords).length);
-l(videos.length);
