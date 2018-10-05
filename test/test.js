3199d54a89894196aeefa347a5f56ddfbfe42688
Jan Blasko <honza@lucien144.net>
2018-10-05 20:27:30 +1000

    AVA, tests WIP
    Signed-off-by: Jan Blasko <honza@lucien144.net>

---
+++ test/test.js
@@ -0,0 +1,16 @@
+import test from 'ava';
+import {parseTitle} from '../helpers';
+
+test('single host', t => {
+	const title = parseTitle('Joe Rogan Experience #144 Jon Jones (part 1)');
+	t.is(title.episode, 144);
+	t.is(title.hosts[0], 'Jon Jones');
+});
+
+test('multiple hosts', t => {
+	const title = parseTitle('Joe Rogan Experience #144 Jon Jones, Elon Musk & Niel deGrasse (part 1)');
+	t.truthy(title.hosts.length, 3);
+	t.is(title.hosts[0], 'Jon Jones');
+	t.is(title.hosts[1], 'Elon Musk');
+	t.is(title.hosts[2], 'Niel deGrasse');
+});
