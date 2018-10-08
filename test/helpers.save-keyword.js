2a9abb77c71303f0a7cb1f1253f5092610df8425
Jan Blasko <honza@lucien144.net>
2018-10-08 23:44:39 +1100

    Test: Save the video along with keyword
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- test/helpers.save-keyword.js
+++ test/helpers.save-keyword.js
@@ -4,14 +4,17 @@ import {saveKeyword} from '../helpers';
 test('saving keyword', t => {
 	const dict = {};
 	const keyword = 'Joe Rogan';
-	saveKeyword(keyword, dict);
+	saveKeyword(keyword, dict, {});
 	t.is(dict.joerogan.count, 1);
 	t.is(dict.joerogan.original, keyword);
+	t.is(dict.joerogan.videos.length, 1);
 
-	saveKeyword(keyword, dict);
+	saveKeyword(keyword, dict, {});
 	t.is(dict.joerogan.count, 2);
+	t.is(dict.joerogan.videos.length, 2);
 
-	saveKeyword(' Joe rogan ', dict);
+	saveKeyword(' Joe rogan ', dict, {});
 	t.is(dict.joerogan.original, keyword, 'Testing case insensitivity + trim');
 	t.is(dict.joerogan.count, 3);
+	t.is(dict.joerogan.videos.length, 3);
 });
