81d93c754244846e23f3d9c2f14c25213f556311
Jan Blasko <honza@lucien144.net>
2018-10-18 22:03:24 +1100

    Searching through hosts
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- server/routes.js
+++ server/routes.js
@@ -1,20 +1,34 @@
 const ObjectID = require('mongodb').ObjectID;
 
 module.exports = function (app, db) {
-	app.get('/hosts', (req, res) => {
-		db.collection('hosts').find().toArray((err, hosts) => {
-			hosts.sort((a, b) => {
-				return a.original < b.original ? -1 : (a.original > b.original ? 1 : 0);
-			});
+	const response = (err, res, hosts, shrunk = true) => {
+		hosts.sort((a, b) => {
+			return a.original < b.original ? -1 : (a.original > b.original ? 1 : 0);
+		});
+		if (shrunk) {
 			hosts.map(a => {
 				a.videos = [];
 				return a;
 			});
-			if (err) {
-				res.send({error: 'An error has occurred'});
-			} else {
-				res.json(hosts);
-			}
+		}
+		if (err) {
+			res.send({error: 'An error has occurred'});
+		} else {
+			res.json(hosts);
+		}
+	};
+
+	app.get('/hosts', (req, res) => {
+		const search = req.query.search;
+		if (search) {
+			const reg = new RegExp(`^${search}`, 'i');
+			db.collection('hosts').find({original: reg}).toArray((err, hosts) => {
+				response(err, res, hosts, false);
+			});
+			return;
+		}
+		db.collection('hosts').find().toArray((err, hosts) => {
+			response(err, res, hosts);
 		});
 	});
 
