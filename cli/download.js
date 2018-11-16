dcc3e59a710687592aee7c99bdad34f218ba4866
Jan Blasko <honza@lucien144.net>
2018-11-16 23:19:21 +1100

    ⛏ Load the video stats
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- cli/download.js
+++ cli/download.js
@@ -10,11 +10,14 @@ const client = new MongoClient(process.env.MONGO_DSN, {useNewUrlParser: true});
 
 const helpers = require('./helpers');
 
+const titles = [];
+const episodes = [];
+
 const syncData = async () => {
 	console.time('execution');
 	await client.connect();
 	const db = client.db(process.env.MONGO_DBNAME);
-	db.collection('videos').deleteMany({});
+	await db.collection('videos').deleteMany({});
 
 	let results = false;
 	let publishedBefore = null;
@@ -32,24 +35,47 @@ const syncData = async () => {
 			console.error(error);
 			return;
 		}
-		results.forEach(video => {
-			if (video.title === undefined) {
-				results = false;
-			} else {
+		await Promise.all(
+			results.map(async video => {
+				// Skip on last item
+				if (video.title === undefined) {
+					results = false;
+					return;
+				}
+
+				publishedBefore = video.publishedAt;
+				publishedBefore.setSeconds(publishedBefore.getSeconds() - 1);
+				publishedBefore = publishedBefore.toISOString();
+
+				// Skip other than JRE podcasts
 				const title = helpers.parseTitle(video.title);
 				if (title === false) {
 					return;
 				}
+
+				// Skip duplicates
+				const id = helpers.getId(title.original);
+				if (episodes.indexOf(title.episode) > -1 || titles.indexOf(id) > -1) {
+					return;
+				}
+				titles.push(title.original);
+				episodes.push(title.episode);
+
+				const {raw} = await API.getVideoByID(video.id, {part: 'statistics'});
+				video.statistics = {
+					viewCount: parseInt(raw.statistics.viewCount, 10),
+					likeCount: parseInt(raw.statistics.likeCount, 10),
+					dislikeCount: parseInt(raw.statistics.dislikeCount, 10),
+					favoriteCount: parseInt(raw.statistics.favoriteCount, 10),
+					commentCount: parseInt(raw.statistics.commentCount, 10)
+				};
+
 				video.title = title;
-				db.collection('videos').insertOne(video);
-				console.log(video.title);
-				console.log(video.publishedAt);
+				await db.collection('videos').insertOne(video);
+				console.log(video.title.original);
 				console.log('---');
-				publishedBefore = video.publishedAt;
-				publishedBefore.setSeconds(publishedBefore.getSeconds() - 1);
-				publishedBefore = publishedBefore.toISOString();
-			}
-		});
+			})
+		);
 	} while (results);
 
 	client.close();
