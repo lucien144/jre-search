a6b1d37ebcd39b571a38c7894bce8043aad5be7f
Jan Blasko <honza@lucien144.net>
2018-12-28 22:56:36 +1100

    Stats moved to new component
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -48,80 +48,18 @@
 			v-model="openStats"
 			@input="v => v || (openStats = false)"
 		>
-			<VCard>
-				<VCardTitle class="headline">
-					Statistics
-				</VCardTitle>
-				<VCardText>
-					<VContainer
-						fluid
-						grid-list-lg
-					>
-						<VLayout>
-							<VFlex xs4>
-								<VCard>
-									<VCardTitle><h4>Top hosts ({{ $store.state.stats.hosts.count }} total)</h4></VCardTitle>
-									<VDivider />
-									<VList dense>
-										<VListTile
-											v-for="(item, index) in $store.state.stats.hosts.top"
-											:key="index"
-											dark
-										>
-											<VListTileContent>
-												<a href="#">
-													{{ item.original }} ({{ item.count }}&times;)
-												</a>
-											</VListTileContent>
-										</VListTile>
-									</VList>
-								</VCard>
-							</VFlex>
-							<VFlex xs4>
-								<VCard>
-									<VCardTitle><h4>Top keywords ({{ $store.state.stats.keywords.count }} total)</h4></VCardTitle>
-									<VDivider />
-									<VList dense>
-										<VListTile
-											v-for="(item, index) in $store.state.stats.keywords.top"
-											:key="index"
-										>
-											<VListTileContent>{{ item.original }} ({{ item.count }}&times;)</VListTileContent>
-										</VListTile>
-									</VList>
-								</VCard>
-							</VFlex>
-							<VFlex xs4>
-								<VCard>
-									<VCardTitle><h4>Top videos ({{ $store.state.stats.videos.count }} total)</h4></VCardTitle>
-									<VDivider />
-									<VList dense>
-										<VListTile
-											v-for="(item, index) in $store.state.stats.videos.top"
-											:key="index"
-										>
-											<VListTileContent>
-												#{{ item.title.episode }} - {{ item.title.hosts.join(', ') }}
-											</VListTileContent>
-											<VListTileContent class="align-end">
-												{{ item.statistics.viewCount }}
-											</VListTileContent>
-										</VListTile>
-									</VList>
-								</VCard>
-							</VFlex>
-						</VLayout>
-					</VContainer>
-				</VCardText>
-			</VCard>
+			<AppStats/>
 		</VDialog>
 	</div>
 </template>
 
 <script>
+import AppStats from '~/components/AppStats.vue';
+
 const netlifyIdentity = require('netlify-identity-widget');
 
 export default {
+	components: { AppStats },
 	data() {
 		return {
 			// Toggle. True if we want to open statistics dialog.
---
+++ components/AppStats.vue
@@ -0,0 +1,77 @@
+<template>
+	<VCard>
+		<VCardTitle class="headline">
+			Statistics
+		</VCardTitle>
+		<VCardText>
+			<VContainer
+				fluid
+				grid-list-lg
+			>
+				<VLayout>
+					<VFlex xs4>
+						<VCard>
+							<VCardTitle><h4>Top hosts ({{ $store.state.stats.hosts.count }} total)</h4></VCardTitle>
+							<VDivider />
+							<VList dense>
+								<VListTile
+									v-for="(item, index) in $store.state.stats.hosts.top"
+									:key="index"
+									dark
+								>
+									<VListTileContent>
+										<a href="#">
+											{{ item.original }} ({{ item.count }}&times;)
+										</a>
+									</VListTileContent>
+								</VListTile>
+							</VList>
+						</VCard>
+					</VFlex>
+					<VFlex xs4>
+						<VCard>
+							<VCardTitle><h4>Top keywords ({{ $store.state.stats.keywords.count }} total)</h4></VCardTitle>
+							<VDivider />
+							<VList dense>
+								<VListTile
+									v-for="(item, index) in $store.state.stats.keywords.top"
+									:key="index"
+								>
+									<VListTileContent>{{ item.original }} ({{ item.count }}&times;)</VListTileContent>
+								</VListTile>
+							</VList>
+						</VCard>
+					</VFlex>
+					<VFlex xs4>
+						<VCard>
+							<VCardTitle><h4>Top videos ({{ $store.state.stats.videos.count }} total)</h4></VCardTitle>
+							<VDivider />
+							<VList dense>
+								<VListTile
+									v-for="(item, index) in $store.state.stats.videos.top"
+									:key="index"
+								>
+									<VListTileContent>
+										#{{ item.title.episode }} - {{ item.title.hosts.join(', ') }}
+									</VListTileContent>
+									<VListTileContent class="align-end">
+										{{ item.statistics.viewCount }}
+									</VListTileContent>
+								</VListTile>
+							</VList>
+						</VCard>
+					</VFlex>
+				</VLayout>
+			</VContainer>
+		</VCardText>
+	</VCard>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
