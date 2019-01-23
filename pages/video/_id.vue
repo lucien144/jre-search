01fc191773aa017400fe75e3e8e3bdfe86ffcf1f
Jan Blasko <honza@lucien144.net>
2019-01-23 23:14:16 +1100

    video detail layout tweeks
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- pages/video/_id.vue
+++ pages/video/_id.vue
@@ -4,7 +4,7 @@
 		id="video"
 	>
 		<VCard>
-			<VToolbar color="white" flat>
+			<VCardTitle>
 				<VBtn
 					icon
 					light
@@ -13,30 +13,51 @@
 					<VIcon color="grey darken-2">fas fa-arrow-left</VIcon>
 				</VBtn>
 				<VToolbarTitle>{{ $store.state.video.title.original }}</VToolbarTitle>
-			</VToolbar>
-			<VContainer fluid grid-list-xl>
-				<VLayout>
-					<VFlex xs12 md8>
-						<iframe
-							:src="`https://www.youtube.com/embed/${$store.state.video.id}?autoplay=1`"
-							width="100%"
-							type="text/html"
-							frameborder="0"
-						/>
-					</VFlex>
-					<VFlex xs12 md4>
-						<p>{{ $store.state.video.description }}</p>
-						<div>
-							<v-chip
-								v-for="(keyword, index) in $store.state.video.keywords"
-								:key="index">{{ keyword }}</v-chip>
-							<v-chip
-								v-for="(hosts, index) in $store.state.video.hosts"
-								:key="index">{{ hosts }}</v-chip>
-						</div>
-					</VFlex>
-				</VLayout>
-			</VContainer>
+			</VCardTitle>
+			<VCardText>
+				<VContainer pt-0 px-3 fluid grid-list-xl>
+					<VLayout>
+						<VFlex xs12 md8>
+							<iframe
+								:src="`https://www.youtube.com/embed/${$store.state.video.id}?autoplay=1`"
+								width="100%"
+								height="100%"
+								type="text/html"
+								frameborder="0"
+							/>
+						</VFlex>
+						<VFlex xs12 md4>
+							<p>{{ $store.state.video.description }}</p>
+
+							<p>
+								<v-chip
+									v-for="(keyword, index) in $store.state.video.keywords"
+									:key="index">{{ keyword }}</v-chip>
+								<v-chip
+									v-for="(hosts, index) in $store.state.video.hosts"
+									:key="index">{{ hosts }}</v-chip>
+							</p>
+
+							<VDivider/>
+							<p>
+								<VChip label color="white">
+									<VIcon left>fas fa-eye</VIcon>{{ $store.state.video.statistics.viewCount | format }}
+								</VChip>
+								<VChip label color="white">
+									<VIcon left>fas fa-thumbs-up</VIcon>{{ $store.state.video.statistics.likeCount | format }}
+								</VChip>
+								<VChip label color="white">
+									<VIcon left>fas fa-thumbs-down</VIcon>{{ $store.state.video.statistics.dislikeCount | format }}
+								</VChip>
+								<VChip label color="white">
+									<VIcon left>fas fa-comment-alt</VIcon>{{ $store.state.video.statistics.commentCount | format }}
+								</VChip>
+							</p>
+
+						</VFlex>
+					</VLayout>
+				</VContainer>
+			</VCardText>
 		</VCard>
 	</div>
 </template>
