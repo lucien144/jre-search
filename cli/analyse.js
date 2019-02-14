4c38a4ddbbc09608869bf5e4385e91c1ea965387
Jan Blasko <honza@lucien144.net>
2019-02-15 08:46:02 +1100

    Reverting async iterator. Causing errors.
    Signed-off-by: Jan Blasko <honza@lucien144.net>

--- cli/analyse.js
+++ cli/analyse.js
@@ -24,7 +24,9 @@ const titles = [];
 	const promiseInserts = [];
 	try {
 		// eslint-disable-next-line no-await-in-loop
-		videos.forEach(video => {
+		while (await videos.hasNext()) {
+			// eslint-disable-next-line no-await-in-loop
+			const video = await videos.next();
 			counter++;
 
 			// 👉 Some episodes are doubled in the list of videos, therefore we need to filter them out.
@@ -54,7 +56,7 @@ const titles = [];
 					)
 				);
 			}
-		});
+		}
 
 		// Mongo: Save all dictionaries to collections
 		const collections = { hosts, tags, keywords };
